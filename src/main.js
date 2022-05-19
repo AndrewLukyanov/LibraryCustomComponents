0c4ee9514b77e39b5a7e0521f1a3fdf1c6931768
AndrewLukyanov <lukyanov.andrew.v@gmail.com>
2022-05-19 22:58:00 +0300

    Create registration for global components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router';
+import Components from './components/index';
 
 const app = createApp(App);
+Components(app);
 app.use(Router);
 app.mount('#app')
