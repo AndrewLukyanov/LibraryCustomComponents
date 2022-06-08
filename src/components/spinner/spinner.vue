a56343f8a954283b9abdd30935d444e73f54efa2
AndrewLukyanov <lukyanov.andrew.v@gmail.com>
2022-06-08 22:39:28 +0300

    Create an spinner component

---
+++ src/components/spinner/spinner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="spinner"
+    :class="classes">
+    <svg viewBox="0 0 50 50">
+      <circle cx="25"
+        cy="25"
+        r="20"
+        fill="transparent"
+        stroke="currentColor"
+        stroke-width="5" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'spinner',
+
+  props: {
+    color: {
+      type: String,
+      default: 'accent'
+    },
+
+    size: {
+      type: String,
+      default: 'md'
+    }
+  },
+
+  computed: {
+    classes() {
+      return {
+        [`spinner--${this.color}`]: this.color,
+        [`spinner--${this.size}`]: this.size
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" src="./spinner.sass" />
