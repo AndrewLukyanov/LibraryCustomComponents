dd21d650eb32950586b48eb0ff00fb59f58926da
AndrewLukyanov <lukyanov.andrew.v@gmail.com>
2022-06-12 12:09:30 +0300

    Create an ui-glyph

---
+++ src/components/ui/glyph/glyph.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="ui-glyph"
+    :class="coloring">
+    <i :class="icon" 
+      :styles="styles" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ui-glyph',
+
+  props: {
+    name: {
+      type: String,
+      default: 'vuejs',
+    },
+
+    size: {
+      type: String,
+      default: '14'
+    },
+
+    color: {
+      type: String,
+      default: 'inherit'
+    }
+  },
+
+  computed: {
+    coloring() {
+      return `ui-glyph--${this.color}`
+    },
+
+    icon() {
+      return `mdi mdi-${this.name}`;
+    },
+
+    styles() {
+      return {
+        [`height: ${this.size}px;`]: this.size,
+        [`width: ${this.size}px;`]: this.size,
+        [`font-size: ${this.size}px;`]: this.size
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" src="./glyph.sass" />
